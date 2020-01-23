a17883f9ca634c84ba33bdff09dcc337d295370b
Armelle <armelle.lecerf@orange.fr>
2020-01-23 19:05:57 +0100

    Ajout de la classe Ax

---
+++ ANTIC GAME/Ax.swift
@@ -0,0 +1,15 @@
+//
+//  Ax.swift
+//  ANTIC GAME
+//
+//  Created by macmini-Armelle on 23/01/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import Foundation
+class Ax : Weapon {
+    init() {
+        super.init (damage : 15)
+        
+    }
+}
