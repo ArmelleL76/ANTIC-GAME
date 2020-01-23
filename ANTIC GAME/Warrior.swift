2a0b95a394f9225bc3fbfe9dc93820883ccd797a
Armelle <armelle.lecerf@orange.fr>
2020-01-23 21:16:45 +0100

    ajout de la classe warrior

---
+++ ANTIC GAME/Warrior.swift
@@ -0,0 +1,52 @@
+//
+//  Warrior.swift
+//  ANTIC GAME
+//
+//  Created by macmini-Armelle on 23/01/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import Foundation
+class Warrior{
+    var fighterName : String
+    var armName : String
+    var arm : Weapon
+    var life : Int
+    
+    
+    init ( fighterName : String, armName : String, arm : Weapon, life : Int)
+    {
+        self.fighterName = fighterName
+        self.armName = armName
+        self.arm = arm
+        self.life = life
+        }
+   
+       
+func receiveDamage(damage : Int){
+            self.life = self.life - damage
+      //If life turns into negative value, life will be equal to zero
+    if self.life < 0
+            {
+                self.life = 0
+            }
+        }
+        
+        
+        
+func fightAgainst(fighter : Warrior)
+        {
+            fighter.receiveDamage(damage : self.arm.damage)
+        }
+func increasePowerArm ()
+        {self.arm.damage += 8}
+        
+        func description() {
+            print("Arm : \(self.armName)" + "   Life : \(self.life)" + "   Damages : \(self.arm.damage)")
+        }
+    func cureOneself(){
+        self.life +=  10 }
+    
+    
+}
+
