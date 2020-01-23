7abcdc4a6ffbda5353059d33a09b417c3940032f
Armelle <armelle.lecerf@orange.fr>
2020-01-23 19:08:19 +0100

    création de la classe Sword

---
+++ ANTIC GAME/Sword.swift
@@ -0,0 +1,15 @@
+//
+//  Sword.swift
+//  ANTIC GAME
+//
+//  Created by macmini-Armelle on 23/01/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import Foundation
+class Sword : Weapon {
+    init() {
+        super.init (damage : 12)
+        
+    }
+}
