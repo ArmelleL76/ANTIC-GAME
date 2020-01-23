96c8c377259a6e762c4b16a4605d487bb2fe8268
Armelle <armelle.lecerf@orange.fr>
2020-01-23 19:13:38 +0100

    création de la classe RandomArm

---
+++ ANTIC GAME/RandomArm.swift
@@ -0,0 +1,15 @@
+//
+//  RandomArm.swift
+//  ANTIC GAME
+//
+//  Created by macmini-Armelle on 23/01/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import Foundation
+class RandomArm : Weapon {
+    init() {
+        super.init (damage : 15)
+        
+    }
+}
