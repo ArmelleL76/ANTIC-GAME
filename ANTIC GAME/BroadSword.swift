1bb1892a139fcbcde72031814050bdabde74ce77
Armelle <armelle.lecerf@orange.fr>
2020-01-23 19:10:33 +0100

    création de la classe broadSword

---
+++ ANTIC GAME/BroadSword.swift
@@ -0,0 +1,15 @@
+//
+//  BroadSword.swift
+//  ANTIC GAME
+//
+//  Created by macmini-Armelle on 23/01/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import Foundation
+class BroadSword : Weapon {
+    init() {
+        super.init (damage : 18)
+        
+    }
+}
