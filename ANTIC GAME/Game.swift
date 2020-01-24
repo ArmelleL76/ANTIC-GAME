bd16707ec24b9f12714972153ab403fa4ac652b0
Armelle <armelle.lecerf@orange.fr>
2020-01-24 17:34:29 +0100

    ajout dans la description de l'apparition du coffre

--- ANTIC GAME/Game.swift
+++ ANTIC GAME/Game.swift
@@ -16,7 +16,7 @@ class Game {
         myText = "*********Welcome to French Game factory!************\n\nYou will test the ANTIC FIGHT GAME: \n\n"
         
         myText += "1. Two players choose three fighters to make a warrior's team. We'll have then two teams.Each player choose a fighter in his own team to meet another fighter in the other team.\n\n"
-        myText += "2. Fighters fight together or choose to improve their arms or to gain more lives .At the end, the winner is the player with a fighter alive\n\n"
+        myText += "2. Fighters fight together or choose to improve their arms or to gain more lives. Sometimes a chest containing a weapon appears and a player randomly choosen can exchange his fighter's arm with the random one .At the end, the winner is the player with a fighter alive\n\n"
         
         myText += "3. Here is the list of possible fighters : \n\n******************\n\nTeam1: \n\nViking1, Gladiator1 and Knight1\n\n******************\n\nTeam2:\n\nViking2, Gladiator2 and Knight2\n\n******************\n\n"
         
