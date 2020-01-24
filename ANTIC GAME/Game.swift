8a5e247fa98710456a2a46affcb2e8e2a9d44145
Armelle <armelle.lecerf@orange.fr>
2020-01-24 17:11:01 +0100

    retrait de deux espaces dans la présentation du jeu

--- ANTIC GAME/Game.swift
+++ ANTIC GAME/Game.swift
@@ -18,7 +18,7 @@ class Game {
         myText += "1. Two players choose three fighters to make a warrior's team\nWe'll have then two teams.Each player choose a fighter in his own team to meet another fighter in the other team.\n\n"
         myText += "Fighters fight together or choose to improve their arms or to gain more lives .At the end, the winner is the player with a fighter alive\n\n"
         
-        myText += "Here is the list of possible fighters : \n\n  Team1: \n\n Viking1, Gladiator1 and Knight1\n\n******************\n\nTeam2:\n\n Viking2, Gladiator2 and Knight2\n\n******************\n\n"
+        myText += "Here is the list of possible fighters : \n\nTeam1: \n\n Viking1, Gladiator1 and Knight1\n\n******************\n\nTeam2:\n\n Viking2, Gladiator2 and Knight2\n\n******************\n\n"
         
         myText += "Here is a list of optionnal names for your warriors :   \nVikings'names : Sigrid, Thor, Bjorn, Odin, Ragnar, Magnum... \nGladiators'names : Lucius, Marcus, Titus, Sextus, Quintus, Servius...\nKnights'names : Gauvain, Arthur, Perceval, Lancelot, Tristan...\n"
    return myText }
