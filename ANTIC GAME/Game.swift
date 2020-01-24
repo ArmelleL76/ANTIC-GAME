91c7756ddf43672141975b87a366417df6bf5787
Armelle <armelle.lecerf@orange.fr>
2020-01-24 17:16:27 +0100

    idem ajout et retrait d'espaces

--- ANTIC GAME/Game.swift
+++ ANTIC GAME/Game.swift
@@ -15,10 +15,10 @@ class Game {
         var myText : String = ""
         myText = "************Welcome to French Game factory!***************\n\nYou will test the ANTIC FIGHT GAME: \n\n"
         
-        myText += "1. Two players choose three fighters to make a warrior's team\nWe'll have then two teams.Each player choose a fighter in his own team to meet another fighter in the other team.\n\n"
-        myText += "Fighters fight together or choose to improve their arms or to gain more lives .At the end, the winner is the player with a fighter alive\n\n"
+        myText += "1. Two players choose three fighters to make a warrior's team. We'll have then two teams.Each player choose a fighter in his own team to meet another fighter in the other team.\n\n"
+        myText += "2. Fighters fight together or choose to improve their arms or to gain more lives .At the end, the winner is the player with a fighter alive\n\n"
         
-        myText += "Here is the list of possible fighters : \n\n******************\n\nTeam1: \n\nViking1, Gladiator1 and Knight1\n\n******************\n\nTeam2:\n\nViking2, Gladiator2 and Knight2\n\n******************\n\n"
+        myText += "3. Here is the list of possible fighters : \n\n******************\n\nTeam1: \n\nViking1, Gladiator1 and Knight1\n\n******************\n\nTeam2:\n\nViking2, Gladiator2 and Knight2\n\n******************\n\n"
         
         myText += "Here is a list of optionnal names for your warriors :   \nVikings'names : Sigrid, Thor, Bjorn, Odin, Ragnar, Magnum... \nGladiators'names : Lucius, Marcus, Titus, Sextus, Quintus, Servius...\nKnights'names : Gauvain, Arthur, Perceval, Lancelot, Tristan...\n"
    return myText }
