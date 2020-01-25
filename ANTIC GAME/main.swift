8b7e4cc7925d115cd0802128481511ad030cabb1
Armelle <armelle.lecerf@orange.fr>
2020-01-25 14:06:40 +0100

    ajout des tableau fighterTeam1 et fighterTeam2 dans le main

--- ANTIC GAME/main.swift
+++ ANTIC GAME/main.swift
@@ -12,11 +12,19 @@ var display = Game()
 print(display.presentation())
 
 //We collect the players'names
-var namePlayer1 : String = collectNamePlayer(number: "first")
-var namePlayer2 : String = collectNamePlayer(number: "second")
+var name = Game()
+var namePlayer1 : String = name.collectNamePlayer(number : "first")
+var namePlayer2 : String = name.collectNamePlayer(number : "second")
 
 //The players give names to their warriors
-
+var fighterTeam1 : [String] = ["","",""]
+for index in 0...2 {
+fighterTeam1[index] += name.collectNameFighter(number: "first")
+}
+var fighterTeam2 : [String] = ["","",""]
+for index in 0...2 {
+fighterTeam2[index] += name.collectNameFighter(number: "second")
+}
 //The players form their team and display teams
 
 
