3b1e7bcecf8a9e778ab3d039a004088773b11c44
Armelle <armelle.lecerf@orange.fr>
2020-02-02 13:21:32 +0100

    modification du main

--- ANTIC GAME/main.swift
+++ ANTIC GAME/main.swift
@@ -12,6 +12,9 @@ var display = Game()
 print(display.presentation())
 
 //We give name to players, players give name to their fighters and we display the teams
+print("\(display.collectNamePlayer())")
+print("\(display.collectNameFighter1())")
+print("\(display.collectNameFighter2())")
 print("\(display.displayWarriorTeam())")
 
 // A chest will appear or not
